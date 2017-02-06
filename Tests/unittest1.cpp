0ffcbf7ea1bf2a07b57d05e669d293c148bf3198
Oliver Tessmer <oliver.tessmer@gmail.com>
2017-02-06 17:43:04 -0500

    added boost to NuGet packages, added code to iterate through solver source dir

--- Tests/unittest1.cpp
+++ Tests/unittest1.cpp
@@ -1,9 +1,13 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
+#include <boost/filesystem.hpp>
+#include <boost/range/iterator_range.hpp>
+#include <iostream>
 
 #include "../Project Euler Solutions/Solver1.cpp"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace boost::filesystem;
 
 namespace Tests
 {		
@@ -13,6 +17,11 @@ namespace Tests
 		
 		TEST_METHOD(TestMethod1)
 		{
+			path solverSourceDir("../Project Euler Solutions/");
+			Assert::IsTrue(is_directory(solverSourceDir), L"Could not find expected solver source directory" );
+			for (auto& entry : boost::make_iterator_range(directory_iterator(solverSourceDir), {}))
+				Logger::WriteMessage(entry.path().c_str()); 
+
 			Solver1 instance = Solver1();
 			int expectedValue = instance.getExpectedTestResult();
 			int actualValue = instance.doTest();
